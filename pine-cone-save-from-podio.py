008d8b8debb45faacf3160f2d92f78798dafd57b
peakdev118 <peakdev118@gmail.com>
2023-03-08 11:00:31 -0500

    modify

--- pine-cone-save-from-podio.py
+++ pine-cone-save-from-podio.py
@@ -123,7 +123,7 @@ cnt= 0
 
 for offset in tqdm(range(0, total, item_cnt_per_page)):
     cnt+=1
-    if(cnt<501):
+    if(cnt<700):
         continue
     items=get_items(app_id, limit=item_cnt_per_page, offset=offset)
     vectors=[]
